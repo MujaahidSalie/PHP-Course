2e4bd03a547df5bce1928ac8ad2fa26760af26b3
Mujaahid Salie <42743914+MujaahidSalie@users.noreply.github.com>
2019-03-11 16:11:55 +0200

    Add files via upload

---
+++ How to show database data on a website using MySQLi/includes/dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+//use root if you using xampp
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
